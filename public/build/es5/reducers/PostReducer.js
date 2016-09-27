ce414183972fb8f834958c3becfa16ea4fd091d8
fullstack360 <info@fullstack360.com>
2016-09-27 07:02:29 -0400

    cleanup

--- public/build/es5/reducers/PostReducer.js
+++ public/build/es5/reducers/PostReducer.js
@@ -38,47 +38,9 @@ module.exports = function (_x, action) {
 		case constants.POSTS_RECEIVED:
 			return update(state, action.posts);
 
-		//			const posts = action.posts
-		//			console.log('POSTS_RECEIVED: '+JSON.stringify(posts))
-		// var newState = Object.assign({}, state)
-		// var array = Object.assign([], newState.postsArray)
-		// var postsMap = Object.assign({}, newState.posts)
-
-		// for (var i=0; i<posts.length; i++){
-		// 	var post = posts[i]
-		// 	if (postsMap[post.slug] != null) // already there
-		// 		continue
-
-		// 	postsMap[post.slug] = post
-		// 	array.push(post)
-		// }
-
-		// newState['postsArray'] = array
-		// newState['posts'] = postsMap
-
-		// return newState
-
 		case constants.USER_POSTS_RECEIVED:
-			//			console.log('USER_POSTS_RECEIVED: '+JSON.stringify(action.posts))
 			return update(state, action.posts);
 
-		// var newState = Object.assign({}, state)
-		// var array = Object.assign([], newState.postsArray)
-		// var postsMap = Object.assign({}, newState.posts)
-
-		// for (var i=0; i<action.posts.length; i++){
-		// 	var post = action.posts[i]
-		// 	if (postsMap[post.slug] != null) // already there
-		// 		continue
-
-		// 	postsMap[post.slug] = post
-		// 	array.push(post)
-		// }
-
-		// newState['postsArray'] = array
-		// newState['posts'] = postsMap
-
-		// return newState
 
 		default:
 			return state;
